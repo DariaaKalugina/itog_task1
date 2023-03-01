4f74d70f16fca272f1fb5dc6738eca84f28ddb13
Daria <macdaria1999@gmail.com>
2023-03-01 22:08:33 +0300

    Add task_itog

---
+++ task_itog/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] array1 = new string[7] {"123", "dot", "54773rt", "))", "hello", "Hi", "1&7"};
+string[] array2 = new string[array1.Length];
+void Function (string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void Print(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Function(array1, array2);
+Print(array2);
+
